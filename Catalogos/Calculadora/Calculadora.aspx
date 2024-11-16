da42c2669982030887214fd57ccaac7b9516cea4
TR Network <TR Network@DESKTOP-NT2PUE0>
2024-11-16 10:20:18 -0600

    Agregar archivos de proyecto.

---
+++ Catalogos/Calculadora/Calculadora.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Calculadora.aspx.cs" Inherits="Cliente_WS.Catalogos.Calculadora.Calculadora" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-4">
+                    <asp:Label ID="lbla" runat="server" Text="Numero 1"></asp:Label>
+                    <asp:TextBox ID="txta" runat="server" TextMode="Number"></asp:TextBox>
+                </div>
+                <br />
+                <<div class="col-4">
+                    <asp:Label ID="lblb" runat="server" Text="Numero 2"></asp:Label>
+                    <asp:TextBox ID="txtb" runat="server" TextMode="Number"></asp:TextBox>
+                </div>
+            </div>
+            <div class="row text-center">
+                <asp:Button ID="btnSumar" runat="server" Text="+" OnClick="btnsumar_Click" CssClass="m-5 col" />
+                <asp:Button ID="btnRestar" runat="server" Text="-" OnClick="btnRestar_Click" CssClass="m-5 col" />
+                <asp:Button ID="btnMultiplicar" runat="server" Text="*" OnClick="btnMultiplicar_Click" CssClass="m-5 col" />
+                <asp:Button ID="btnDividir" runat="server" Text="/" OnClick="btnDividir_Click" CssClass="m-5 col" />
+            </div>
+            <div class="row">
+                <div class="text-center">
+                    <h2>
+                        <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </asp:Content>
---
+++ Catalogos/Calculadora/Calculadora.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Cliente_WS.Calculadora_Service;
+
+namespace Cliente_WS.Catalogos.Calculadora
+{
+    public partial class Calculadora : System.Web.UI.Page
+    {
+        calculadora_ServiceSoapClient clientes_WS;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            clientes_WS =new calculadora_ServiceSoapClient();
+        }
+
+        protected void btnsumar_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+
+            double resultado = clientes_WS.Suma(a,b);
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+        protected void btnRestar_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+
+            double resultado = clientes_WS.Resta(a, b);
+            lblResultado.Text = resultado.ToString();
+        }
+
+        protected void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+
+            double resultado = clientes_WS.Multiplicar(a, b);
+            lblResultado.Text = resultado.ToString();
+        }
+
+        protected void btnDividir_Click(object sender, EventArgs e)
+        {
+
+            double a = double.Parse(txta.Text);
+            double b = double.Parse(txtb.Text);
+
+            double resultado = clientes_WS.Resta(a, b);
+            lblResultado.Text = resultado.ToString();
+        }
+    }
+}

c5485d29473a21a69a7088a4707343f24d5b9e37
TR Network <TR Network@DESKTOP-NT2PUE0>
2024-11-22 17:46:28 -0600

    terminado

---
+++ Catalogos/Camiones/listar_Camiones.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="listar_Camiones.aspx.cs" Inherits="Cliente_WS.Catalogos.Camiones.listar_Camiones" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+    <div class="row">
+        <h3>Lista de camiones</h3>
+        <p>
+            <asp:Button ID="Insertar" runat="server" Text="Agregar" CssClass="btn btn-primary btn-xs" Width="85px" OnClick="Insertar_Click" />
+        </p>
+    </div>
+    <div class="row">
+        <asp:GridView ID="GVCamiones" runat="server" CssClass="table table-bordered table-striped table-condensed" AutoGenerateColumns="false" DataKeyNames="Id_Camion" OnRowDeleting="GVCamiones_RowDeleting" OnRowCommand="GVCamiones_RowCommand" OnRowEditing="GVCamiones_RowEditing" OnRowUpdating="GVCamiones_RowUpdating" OnRowCancelingEdit="GVCamiones_RowCancelingEdit">
+            <Columns>
+                <asp:BoundField DataField="Id_Camion" HeaderText="Numero de Camion" ItemStyle-Width="85px" ReadOnly="true"></asp:BoundField>
+                <asp:BoundField DataField="Matricula" HeaderText="Matricula" ItemStyle-Width="85px"></asp:BoundField>
+                <asp:BoundField DataField="Tipo_camion" HeaderText="Tipo Camion" ItemStyle-Width="85px"></asp:BoundField>
+                <asp:TemplateField HeaderText="Disponibilidad" ItemStyle-Width="50px">
+                    <itemtemplate>
+                        <div style="width: 100%">
+                            <div style="width: 25%; margin: 0 auto">
+                                <asp:CheckBox ID="chkDisponible" runat="server" Checked='<%#Eval("Disponibilidad") %>' Enabled="false" />
+                            </div>
+                        </div>
+                    </itemtemplate>
+                    <edititemtemplate>
+                        <div style="width: 100%">
+                            <div style="width: 25%; margin: 0 auto">
+                                <asp:CheckBox ID="chkEditDisponible" runat="server" Checked='<%#Eval("Disponibilidad") %>' />
+                            </div>
+                        </div>
+                    </edititemtemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="UrlFoto" HeaderText="UrlFoto" ItemStyle-Width="85px"></asp:BoundField>
+                <asp:ImageField HeaderText="Foto" ReadOnly="true" ItemStyle-Width="120px" ControlStyle-Height="120px" ControlStyle-Width="120px" DataImageUrlField="UrlFoto"></asp:ImageField>
+                <asp:ButtonField ButtonType="Button" CommandName="Select" HeaderText="1" text="Ver detalles" ControlStyle-CssClass="btn btn-primary btn-xs" ItemStyle-Width="50px"></asp:ButtonField>
+                <asp:CommandField ButtonType="Button" HeaderText="2" ShowEditButton="true" ShowHeader="true" ControlStyle-CssClass="btn btn-warning btn-xs" ItemStyle-Width="50px"></asp:CommandField>
+                <asp:CommandField ButtonType="Button" HeaderText="3" ShowDeleteButton="true" ShowHeader="true" ControlStyle-CssClass="btn btn-danger btn-xs" ItemStyle-Width="50px"></asp:CommandField>
+
+            </Columns>
+        </asp:GridView>
+
+    </div>
+</div>
+</asp:Content>
---
+++ Catalogos/Camiones/listar_Camiones.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Cliente_WS.Camiones_Service;
+using Cliente_WS.Utilidades;
+
+namespace Cliente_WS.Catalogos.Camiones
+{
+    public partial class listar_Camiones : System.Web.UI.Page
+    {
+        camiones_ServiceSoapClient camiones_Client_WS;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            camiones_Client_WS = new camiones_ServiceSoapClient();
+
+            if (!IsPostBack)
+            {
+                cargarGrid();
+
+            }
+        }
+        public void cargarGrid()
+        {
+            //carga info desde bll al gv
+            GVCamiones.DataSource = camiones_Client_WS.GetCamiones(new ArrayOfAnyType { });
+            GVCamiones.DataBind();
+        }
+        protected void Insertar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("fornulariocamiones.aspx");
+        }
+
+        protected void GVCamiones_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Select")
+            {
+                int varIndex = int.Parse(e.CommandArgument.ToString());
+                String id = camiones_Client_WS.DataKeys[varIndex].Values["Id_Camion"].ToString();
+                Response.Redirect($"fornulariocamiones.aspx?Id={id}");
+
+            }
+        }
+
+        protected void GVCamiones_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GVCamiones.EditIndex = e.NewEditIndex;
+            cargarGrid();
+        }
+
+        protected void GVCamiones_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int idcamion = int.Parse(GVCamiones.DataKeys[e.RowIndex].Values["Id_Camion"].ToString());
+            string matricula = e.NewValues["Matricula"].ToString();
+            string Tipocamion = e.NewValues["Tipo_camion"].ToString();
+            string Foto = e.NewValues["UrlFoto"].ToString();
+            CheckBox chaux = (CheckBox)GVCamiones.Rows[e.RowIndex].FindControl("chkEditDisponible");
+            bool disponibilidad = chaux.Checked;
+            Camiones_VO _camion = camiones_Client_WS.GetCamiones(new ArrayOfAnyType { "@Id_Camion", idcamion })[0];
+            Camiones_VO _camionAux = new Camiones_VO();
+            _camionAux.Id_Camion = idcamion;
+            _camionAux.Matricula = matricula;
+            _camionAux.Disponibilidad = disponibilidad;
+            _camionAux.Tipo_Camion = Tipocamion;
+            _camionAux.UrlFoto = Foto;
+            _camionAux.Marca = _camion.Marca;
+            _camionAux.Modelo = _camion.Modelo;
+            _camionAux.Capacidad = _camion.Capacidad;
+            _camionAux.Kilometraje = _camion.Kilometraje;
+            //sweetAlert
+            string respuesta = "";
+            string titulo, msg, tipo;
+            try
+            {
+                respuesta = camiones_Client_WS.ActualizarCamion(_camionAux);
+                if (respuesta.ToUpper().Contains("Error"))
+                {
+                    titulo = "Error";
+                    msg = respuesta;
+                    tipo = "error";
+                }
+                else
+                {
+                    titulo = "Correcto";
+                    msg = respuesta;
+                    tipo = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                titulo = "Opss..";
+                msg = ex.Message;
+                tipo = "error";
+            }
+        }
+
+        protected void GVCamiones_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GVCamiones.EditIndex = -1;
+            cargarGrid();
+        }
+
+        protected void GVCamiones_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int idcamion = int.Parse(GVCamiones.DataKeys[e.RowIndex].Values["Id_Camion"].ToString());
+            string respuesta = camiones_Client_WS.EliminarCamion(idcamion);
+            string titulo, msg, tipo;
+            if (respuesta.ToUpper().Contains("Error"))
+            {
+                titulo = "Error";
+                msg = respuesta;
+                tipo = "error";
+            }
+            else
+            {
+                titulo = "Correcto";
+                msg = respuesta;
+                tipo = "success";
+            }
+            sweetAlert.Sweet_Alert(titulo, msg, tipo, this.Page, this.GetType());
+            cargarGrid();
+        }
+    }
+}
